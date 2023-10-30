c679064eda1851bcdf5878d8ed6c92964de6ed06
AsmaeCharr <asmaecharroudd@gmail.com>
2023-10-30 10:21:10 +0100

    fixed some cursor errors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let x = document.querySelector(".cursor");
-let density = 2,
-    number = 40; // length of the trail
+let density = 1,
+    number = 50; // length of the trail
 for (let i = 0; i < number; i++) {
     let y = document.createElement("div");
     y.classList.add("circle");
@@ -21,17 +21,17 @@ document.addEventListener("mousemove", ({ clientX, clientY }) => {
 document.addEventListener("mouseover", (e) => {
     console.log(e.target.tagName)
     if (e.target.tagName == 'A' || e.target.tagName == 'BUTTON' || e.target.tagName == 'INPUT' || e.target.tagName == 'LI' || e.target.tagName == 'IMG') {
-        document.getElementById('circle').style.height = '1.5rem';
-        document.getElementById('circle').style.width = '1.5rem';
+        document.getElementById('circle').style.height = '2rem';
+        document.getElementById('circle').style.width = '2rem';
         document.getElementById('circle').style.background = '#000';
-        document.getElementById('circle').style.opacity = '0.5';
+        document.getElementById('circle').style.opacity = '0.3';
     }
 });
 
 document.addEventListener("mouseout", (e) => {
     if (e.target.tagName == 'A' || e.target.tagName == 'BUTTON' || e.target.tagName == 'INPUT' || e.target.tagName == 'LI' || e.target.tagName == 'IMG') {
-        document.getElementById('circle').style.height = '1rem';
-        document.getElementById('circle').style.width = '1rem';
+        document.getElementById('circle').style.height = '0.6rem';
+        document.getElementById('circle').style.width = '0.6rem';
         document.getElementById('circle').style.opacity = '1';
         document.getElementById('circle').style.background = '#000';
     }
